c256f99b46a346c966c62853c0e5b44a894668d5
Rafael Augusto <rafaelvicio@icloud.com>
2017-04-17 10:12:35 -0300

    Aula 9 - Criando Filtros

---
+++ js/filters/elipsisFilter.js
@@ -0,0 +1,8 @@
+angular.module("listaTelefonica").filter("elipsis", function() {
+  return function(input, size) {
+
+    if (input.length <= size) return input;
+    var output = input.substring(0,(size || 2)) + "...";
+    return output;
+  };
+});
